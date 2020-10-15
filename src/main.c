eef649fbd063bfbf034afe91d1e26b5ed5c4a0e4
Rahul Garg <rahulyugalgarg@gmail.com>
2020-10-15 23:49:15 +0530

    [Added] Gpio initialisation sequence and toggle function
    Comment for intialisation sequence of GPIO and init function
    configures onboard led of stm32f7 nucleo board in output mode
    small routine is placed in main function to check the configuration.
    
    Signed-off-by: Rahul Garg <rahulyugalgarg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -6,8 +6,16 @@
  */
 
 #include <stdint.h>
+#include "stm32f746xx.h"
 
 int main()
 {
+	uint32_t itr1,itr2;
+	init_gpio();
+	while(1){
+		gpio_toggle(GPIOB,0);
+		for(itr1 = 0; itr1 < 1000; itr1++)
+			for( itr2 = 0; itr2 < 1000; itr2++ );
+	}
 	return 0;
 }
