821746edf0f9d3081f67aee44c4d46bb07706de2
Rahul Garg <rahulyugalgarg@gmail.com>
2020-10-18 20:34:21 +0530

    [Added] usrt_rx_newline function
    This function returns pointer of string terminated
    by newline character recived on uart.
    
    It compares the every character received on UART
    with newline and returns on true condition.
    
    Signed-off-by: Rahul Garg <rahulyugalgarg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -23,14 +23,17 @@ void uart_tx(char *data){
 	return;
 }
 
-char * uart_rx(uint8_t length){
-	uint8_t var;
+char * uart_rx_newline(){
+	uint8_t var = 0;
 	static char buff[MAX_UART_RX];
 	USART_TypeDef *pUSART_Handle;
 	pUSART_Handle = USART2;
-	for (int var = 0; var < length ; ++var) {
+	while(1) {
 		while(!(pUSART_Handle->ISR & (USART_ISR_RXNE)));
 		buff[var] = pUSART_Handle->RDR;
+		if(buff[var] == '\n')
+			break;
+		var++;
 	}
 	return buff;
 }
@@ -45,7 +48,7 @@ int main()
 		if( gpio_read(GPIOC,13) ){
 			gpio_toggle(GPIOB,0);
 		}
-		buff = uart_rx(11);
+		buff = uart_rx_newline();
 		uart_tx(buff);
 		for(itr1 = 0; itr1 < 1000; itr1++)
 			for( itr2 = 0; itr2 < 1000; itr2++ );
