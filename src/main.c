5a92cac6509bee40c955a88f944698858a01c019
Rahul Garg <rahulyugalgarg@gmail.com>
2020-10-18 18:54:31 +0530

    [Added] Send hello world on USART2
    Checking TXE status bit before transmitting on uart.
    And when TXE is one writting data on TDR for new data,
    to be sent.
    
    Signed-off-by: Rahul Garg <rahulyugalgarg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -8,6 +8,18 @@
 #include <stdint.h>
 #include "stm32f746xx.h"
 
+void uart_tx(){
+	uint8_t var;
+	USART_TypeDef *pUSART_Handle;
+	pUSART_Handle = USART2;
+	char data[12]="hello world\n";
+	for (var = 0; var < 12; ++var) {
+		while(!(pUSART_Handle->ISR & (USART_ISR_TXE)));
+		pUSART_Handle->TDR = data[var];
+	}
+	return;
+}
+
 int main()
 {
 	uint32_t itr1,itr2;
@@ -16,6 +28,7 @@ int main()
 	while(1){
 		if( gpio_read(GPIOC,13) ){
 			gpio_toggle(GPIOB,0);
+			uart_tx();
 		}
 		for(itr1 = 0; itr1 < 1000; itr1++)
 			for( itr2 = 0; itr2 < 1000; itr2++ );
