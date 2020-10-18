4761013065d0f03690dfb923cd65ee13836e5ed5
Rahul Garg <rahulyugalgarg@gmail.com>
2020-10-18 10:53:25 +0530

    [Added] UART init steps in comment
    This comment contains uart peripheral initialisation
    process with all combination
    
    Signed-off-by: Rahul Garg <rahulyugalgarg@gmail.com>

--- Driver/stm32f7xx_uart.c
+++ Driver/stm32f7xx_uart.c
@@ -5,7 +5,33 @@
  *      Author: RahulGarg
  */
 
+/*
+ * @brief :
+ *
+ * @note :
+ * 	The UART HAL driver can be used as follows:
+ *		1. Declare a UART_HandleTypeDef handle structure.
+ *		2. Initialize the UART low level resources :
+ *			a. Enable the USARTx interface clock.
+ *			b. UART pins configuration:
+ *				- Enable the clock for the UART GPIOs.
+ *				- Configure these UART pins as alternate function pull-up.
+ *			c. NVIC configuration if you need to use interrupt process :
+ *				- Configure the USARTx interrupt priority.
+ *				- Enable the NVIC USART IRQ handle.
+ *			d. DMA Configuration if you need to use DMA process:
+ *				- Declare a DMA handle structure for the Tx/Rx stream.
+ *				- Enable the DMAx interface clock.
+ *				- Configure the declared DMA handle structure with the required Tx/Rx parameters.
+ *				- Configure the DMA Tx/Rx Stream.
+ *				- Associate the initialized DMA handle to the UART DMA Tx/Rx handle.
+ *				- Configure the priority and enable the NVIC for the transfer complete interrupt on the DMA Tx/Rx Stream.
+ *		3. Program the Baud Rate, Word Length, Stop Bit, Parity,Hardware flow control and Mode(Receiver/Transmitter) in the Init structure.
+ *		4. For the UART asynchronous mode, initialize the UART registers.
+ *
+ */
 void init_uart(void){
+
 	return;
 }
 
